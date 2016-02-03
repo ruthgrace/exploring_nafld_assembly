00220e911d5b003030870ddb9262aa619210f65f
ruthgrace <ruthgracewong@gmail.com>
2016-02-03 18:15:41 -0500

    added length check debug statements

--- remove_refseqs.pl
+++ remove_refseqs.pl
@@ -34,6 +34,9 @@ while(defined(my $l = <BLAST>)){
   $start = $items[6];
   $end = $items[7];
   $qlen = $items[12];
+  print "start: $start, end: $end, qlen: $qlen, abs(start-end)/qlen ";
+  print abs($start - $end) / $qlen;
+  print "\n";
   if (abs($start - $end) / $qlen >= 0.9) {
     $badseqs{$trinityseqid} = "trinity seq id";
   }
