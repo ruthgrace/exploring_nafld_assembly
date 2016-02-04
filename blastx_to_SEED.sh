1f3e8208616054719a143616e644aa0ee8968b2a
ruthgrace <ruthgracewong@gmail.com>
2016-02-03 19:49:54 -0500

    added blastx to seed, need to debug

---
+++ blastx_to_SEED.sh
@@ -0,0 +1,2 @@
+blastx -db ../../../SEED_database/db_fastas.complex_plusold.faa -query ./CL_119_R1_assembled_no_refseqs.fa -out CL_119_R1_SEED_blast.out -outfmt 6 -evalue 1e-3 -num_alignments 10 -num_threads 4
+
