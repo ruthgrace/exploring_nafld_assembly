696b826a32e5531566551a3827192cbd776a863f
ruthgrace <ruthgracewong@gmail.com>
2016-03-12 01:29:41 -0500

    wrote script for adding sample names to all no_refseq assembled files (need to test/debug)

---
+++ add_all_samples_name_to_no_refseq.pl
@@ -0,0 +1,6 @@
+for d in ../scripts/trinity_* ; do
+  trinityFasta="$d/Trinity.fasta";
+  sample=${d:19};
+  noRefseqs=$sample"/"$sample"_assembled_no_refseqs.fa";
+  perl add_sample_name_to_no_refseq.pl $sample"/"$sample"_assembled_no_refseqs.fa" $sample > $sample"/"$sample"_assembled_no_refseqs_with_sample_name.fa"
+done
