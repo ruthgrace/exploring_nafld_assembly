cbd6a18004a318fc2f2c9a28c28a105463ecf113
ruthgrace <ruthgracewong@gmail.com>
2016-03-12 12:52:37 -0500

    wrote script for getting seed hierarchy from all annotated assembled files per sample (need to test/debug)

---
+++ get_all_seed_hier.sh
@@ -0,0 +1,10 @@
+dir=$1; # first parameter is the directory with one folder per sample
+for D in `find $dir -type d`
+do
+    sample=$(basename $D);
+    echo "processing $sample";
+    seed_blast_out=$sample"/"$sample"_SEED_blast.out";
+    perl ../../scripts/get_subsys4.pl -i $seed_blast_out > $sample"/"$sample"_get_subsys4_output.txt";
+    perl ../../scripts/get_seed_hier.pl -i $sample"/"$sample"_get_subsys4_output.txt" > $sample"/"$sample"_get_seed_hier_output.txt";
+    perl add_sample_name_to_seed_hier_output.pl $sample"/"$sample"_get_seed_hier_output.txt" $sample > $sample"/"$sample"_get_seed_hier_output_with_sample_name.txt";
+done
