418991bf5bd30f0f72b0bbaabc700e616b83363b
ruthgrace <ruthgracewong@gmail.com>
2016-03-28 19:41:11 -0400

    added funcitonality

--- remove_path_from_all_sam.sh
+++ remove_path_from_all_sam.sh
@@ -1,9 +1,10 @@
 
 for d in "$1"/*map
 do
-  echo $d;
-  echo ${d##*/};
   sample=${d##*/};
   sample=${sample::-4};
   echo $sample;
+  samfile=$d/$sample.sam;
+  samfile_no_path=$d/$sample"_no_path.sam";
+  ./remove_path_from_sam.pl $samfile $samfile_no_path;
 done
