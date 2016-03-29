4ce99483dc551bdd1731f7abd4bcc6847c3baf65
ruthgrace <ruthgracewong@gmail.com>
2016-03-28 20:08:33 -0400

    debugged

--- remove_path_from_all_blast_out.sh
+++ remove_path_from_all_blast_out.sh
@@ -6,7 +6,6 @@ do
     echo $sample;
     blastfile=$d/$sample"_SEED_blast.out";
     blastfile_no_path=$d/$sample"_SEED_blast_no_path.out";
-#    ./remove_path_from_blast_out.pl $blastfile $blastfile_no_path;
+    ./remove_path_from_blast_out.pl $blastfile $blastfile_no_path;
   fi
-  echo "not dir "$D;
 done
