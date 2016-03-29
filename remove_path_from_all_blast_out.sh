993b513911cd5759cb73d8d537d9d61ddcbcc9a1
ruthgrace <ruthgracewong@gmail.com>
2016-03-28 20:09:09 -0400

    debugged some more

--- remove_path_from_all_blast_out.sh
+++ remove_path_from_all_blast_out.sh
@@ -4,8 +4,8 @@ do
   if [ -d "${D}" ]; then
     sample=${D##*/};
     echo $sample;
-    blastfile=$d/$sample"_SEED_blast.out";
-    blastfile_no_path=$d/$sample"_SEED_blast_no_path.out";
+    blastfile=$D/$sample"_SEED_blast.out";
+    blastfile_no_path=$D/$sample"_SEED_blast_no_path.out";
     ./remove_path_from_blast_out.pl $blastfile $blastfile_no_path;
   fi
 done
