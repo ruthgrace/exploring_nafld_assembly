cf6c2db8f1a0e949756b2882f86872be283580fd
ruthgrace <ruthgracewong@gmail.com>
2016-04-02 05:21:12 -0400

    perl

--- add_all_sample_names_to_refseq_matches.sh
+++ add_all_sample_names_to_refseq_matches.sh
@@ -1,5 +1,5 @@
 for d in "$1"/*/ ; do
   sample=$(basename $d);
   echo "processing $sample";
-  nohup add_sample_name_to_no_refseq.pl $d$sample"_refseq_matches.fasta" $d$sample"_refseq_matches_with_sample_name.fasta" &
+  nohup perl add_sample_name_to_no_refseq.pl $d$sample"_refseq_matches.fasta" $d$sample"_refseq_matches_with_sample_name.fasta" &
 done
