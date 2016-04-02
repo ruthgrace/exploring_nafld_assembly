c970c63be9c37c9d1f4ec6695831e9450f9cb3a7
ruthgrace <ruthgracewong@gmail.com>
2016-04-02 16:25:19 -0400

    added debug prints to recursive blast

--- recursive_blast_to_seed.sh
+++ recursive_blast_to_seed.sh
@@ -11,12 +11,14 @@ for d in "$1"/[CH]*/ ; do
     echo "recursion round "$counter", seqfile size "$seqsize;
     counter=$((counter+1));
     blastout=$d$sample"_"$nonmatch"_"$counter"_SEED_blast.out";
+    echo "blast with "$nonmatchfile", output "$blastout;
     #blastx
     blastx -db ../../../SEED_database/db_fastas.complex_plusold.faa -query $nonmatchfile -out $blastout -outfmt 6 -evalue 1e-3 -num_alignments 100 -num_threads 44
     #extract matches and non matches
     newnonmatchfile=$d$sample"_"$nonmatch"_"$counter".fasta";
     matchfile=$d$sample"_"$match"_"$counter".fasta";
     matchblastout=$d$sample"_"$match"_"$counter"_SEED_BLAST.out";
+    echo "extract ref seed seqs from "$blastout" and "$nonmatchfile", output to "$matchfile" and "$newnonmatchfile;
     perl extract_ref_seed_seqs.pl $blastout $nonmatchfile $matchfile $newnonmatchfile $matchblastout;
     #update seqsize for non matches file
     nonmatchfile=$newnonmatchfile;
