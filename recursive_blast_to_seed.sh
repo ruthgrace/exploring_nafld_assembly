6525caa7878f72af40f626fdc883b97ea1a60a05
ruthgrace <ruthgracewong@gmail.com>
2016-04-04 15:00:51 -0400

    recurse if file > 0 not file >= 0

--- recursive_blast_to_seed.sh
+++ recursive_blast_to_seed.sh
@@ -1,27 +1,29 @@
 for d in "$1"/[CH]*/ ; do
   sample=$(basename $d);
-  echo "processing $sample";
-  counter=1;
-  nonmatch="refseq_non_matches";
-  match="refseq_matches";
-  nonmatchfile=$d$sample"_"$nonmatch"_"$counter".fasta";
-  cp $d$sample"_"$nonmatch".fasta" $nonmatchfile;
-  seqsize=$(wc -c <"$nonmatchfile")
-  while [ $seqsize -ge 0 ]; do
-    echo "recursion round "$counter", seqfile size "$seqsize;
-    counter=$((counter+1));
-    blastout=$d$sample"_"$nonmatch"_"$counter"_SEED_blast.out";
-    echo "blast with "$nonmatchfile", output "$blastout;
-    #blastx
-    blastx -db ../../../SEED_database/db_fastas.complex_plusold.faa -query $nonmatchfile -out $blastout -outfmt 6 -evalue 1e-3 -num_alignments 100 -num_threads 44
-    #extract matches and non matches
-    newnonmatchfile=$d$sample"_"$nonmatch"_"$counter".fasta";
-    matchfile=$d$sample"_"$match"_"$counter".fasta";
-    matchblastout=$d$sample"_"$match"_"$counter"_SEED_BLAST.out";
-    echo "extract ref seed seqs from "$blastout" and "$nonmatchfile", output to "$matchfile" and "$newnonmatchfile;
-    perl extract_ref_seed_seqs.pl $blastout $nonmatchfile $matchfile $newnonmatchfile $matchblastout;
-    #update seqsize for non matches file
-    nonmatchfile=$newnonmatchfile;
+  if [ sample != "CL_119_R1" ]; then
+    echo "processing $sample";
+    counter=1;
+    nonmatch="refseq_non_matches";
+    match="refseq_matches";
+    nonmatchfile=$d$sample"_"$nonmatch"_"$counter".fasta";
+    cp $d$sample"_"$nonmatch".fasta" $nonmatchfile;
     seqsize=$(wc -c <"$nonmatchfile")
-  done
+    while [ $seqsize > 0 ]; do
+      echo "recursion round "$counter", seqfile size "$seqsize;
+      counter=$((counter+1));
+      blastout=$d$sample"_"$nonmatch"_"$counter"_SEED_blast.out";
+      echo "blast with "$nonmatchfile", output "$blastout;
+      #blastx
+      blastx -db ../../../SEED_database/db_fastas.complex_plusold.faa -query $nonmatchfile -out $blastout -outfmt 6 -evalue 1e-3 -num_alignments 100 -num_threads 44
+      #extract matches and non matches
+      newnonmatchfile=$d$sample"_"$nonmatch"_"$counter".fasta";
+      matchfile=$d$sample"_"$match"_"$counter".fasta";
+      matchblastout=$d$sample"_"$match"_"$counter"_SEED_BLAST.out";
+      echo "extract ref seed seqs from "$blastout" and "$nonmatchfile", output to "$matchfile" and "$newnonmatchfile;
+      perl extract_ref_seed_seqs.pl $blastout $nonmatchfile $matchfile $newnonmatchfile $matchblastout;
+      #update seqsize for non matches file
+      nonmatchfile=$newnonmatchfile;
+      seqsize=$(wc -c <"$nonmatchfile")
+    done
+  fi
 done
