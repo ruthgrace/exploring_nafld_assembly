9ca9b37301d527cd2028aaed1df117645a2f5670
ruthgrace <ruthgracewong@gmail.com>
2016-04-21 22:43:05 -0400

    fixed filename output

--- add_all_sample_names_to_refseq_match_fasta.sh
+++ add_all_sample_names_to_refseq_match_fasta.sh
@@ -1,5 +1,5 @@
 for d in "$1"/*/ ; do
   sample=$(basename $d);
   echo "processing $sample";
-  nohup perl add_sample_name_to_no_refseq.pl $d$sample"_all_refseq_matches.fasta" $sample > $d$sample"CL_119_R1_all_refseq_matches_with_sample_name.fasta" 2>&1&
+  nohup perl add_sample_name_to_no_refseq.pl $d$sample"_all_refseq_matches.fasta" $sample > $d$sample"_all_refseq_matches_with_sample_name.fasta" 2>&1&
 done
