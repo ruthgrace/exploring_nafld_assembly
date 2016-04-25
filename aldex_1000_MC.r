daa6457fb2f0ab193000dd61e2d1ab0f97d8ceab
ruthgrace <ruthgracewong@gmail.com>
2016-04-25 14:01:00 -0400

    made aldex 1000 mc runnable by rscript

--- aldex_1000_MC.r
+++ aldex_1000_MC.r
@@ -1,3 +1,4 @@
+#!/usr/bin/env Rscript
 
 nash <- c("CL_166_BL_R2_R1", "CL_169_BL_R1", "CL_139_BL_2_R1", "CL_173_2_R1", "CL_144_2_R1", "CL_177_R1", "CL_160_R1", "CL_165_R1", "CL_119_R1", "CL_141_BL_R2_R1")
 healthy <- c("HLD_100_R1", "HLD_102_R1", "HLD_111_2_R1", "HLD_80_R1", "HLD_85_R1", "HLD_28_R1", "HLD_47_R1", "HLD_72_2_R1", "HLD_112_R1", "HLD_23_R1")
